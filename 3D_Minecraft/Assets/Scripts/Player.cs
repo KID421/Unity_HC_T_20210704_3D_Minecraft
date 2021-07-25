98538b48035b60f6d9ee43b714c65e2de91d33b0
KID <50666207+KID421@users.noreply.github.com>
2021-07-25 17:10:07 +0800

    使用裝備、建造地形物件系統

--- 3D_Minecraft/Assets/Scripts/Player.cs
+++ 3D_Minecraft/Assets/Scripts/Player.cs
@@ -16,6 +16,10 @@ public class Player : MonoBehaviour
     /// 道具管理器
     /// </summary>
     private Inventory inventory;
+    /// <summary>
+    /// 裝備管理器
+    /// </summary>
+    private EquipmentManager equipmentManage;
 
     private void Start()
     {
@@ -25,11 +29,13 @@ public class Player : MonoBehaviour
         traCamera = GameObject.Find("攝影機").transform;
 
         inventory = GameObject.Find("道具管理器").GetComponent<Inventory>();
+        equipmentManage = GameObject.Find("裝備管理器").GetComponent<EquipmentManager>();
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
+        // 開發裝備系統時先隱藏
+        Gizmos.color = Color.clear;
         // 圖示.繪製射線(中心點，射線方向)
         Gizmos.DrawRay(traCamera.position, traCamera.forward * rangeCollection);
     }
@@ -74,6 +80,7 @@ public class Player : MonoBehaviour
         if (prop.tag == "可以吃的道具")
         {
             inventory.AddProp(prop.GetComponent<Prop>());
+            equipmentManage.ShowEquipment();
         }
     }
     #endregion
